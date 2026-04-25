9f58ff16450c8c041321cf278fa2719e1e450489
obijuan <obijuan.cube@gmail.com>
2026-04-25 22:50:12 +0200

    log: update example 11:  leds_buttons

--- wiki/log/examples/11-leds_buttons/leds_buttons.v
+++ wiki/log/examples/11-leds_buttons/leds_buttons.v
@@ -5,10 +5,17 @@
 
 module leds_buttons (
     input wire [4:0] buttons, 
-    output wire [4:0] leds
+    output wire [15:0] leds
 );
 
 //-- Sacar los botones por los leds
-assign leds = buttons;
+assign leds[15:11] = buttons;
+
+
+//-- BUG!
+//-- Si se asignan valores a los leds restantes
+//-- se obtiene este error
+//-- ERROR: Invalid global constant node 'INT_L_X0Y3/GND_WIRE'
+//assign leds[10:0] = 11'h0;
 
 endmodule
