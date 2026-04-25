f1b2282e76be8cdbcff650ec74d9d52320b969d8
obijuan <obijuan.cube@gmail.com>
2026-04-26 00:30:45 +0200

    log: add example 13:  button_edges

---
+++ wiki/log/examples/13-button_edges/button_edges.v
@@ -0,0 +1,40 @@
+`default_nettype none   
+
+//-- Contar los flancos de subida en un pulsador
+//-- (Incluidos los que llegan por rebotes)
+module button_edges (
+    input wire clk, 
+    input wire [4:0] buttons, 
+    output wire [15:0] leds
+);
+
+//------- Sincronizador para el pulsador
+//-- Pulsador sincronizado
+wire button_sync;
+synchronizer u_sync (
+    .clk(clk),
+    .async_in(buttons[0]),
+    .sync_out(button_sync)
+);
+
+//---- Detectar flancos en el pulsador
+wire button_action;
+edge_detector u_edge (
+    .clk(clk),
+    .value(button_sync),
+    .edges(button_action)
+);
+
+//-- Contador de pulsaciones y liberaciones
+//-- (Cuenta también los rebotes)
+reg [15:0] cont;
+always @(posedge clk) begin
+    if (button_action)
+      cont <= cont + 1; 
+end
+
+//-- Mostrar el contador en los leds
+assign leds = cont;
+
+endmodule
+
---
+++ wiki/log/examples/13-button_edges/upload.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+NAME=button_edges
+
+apio raw -- openFPGALoader --board basys3 --bitstream $NAME.bit
+
