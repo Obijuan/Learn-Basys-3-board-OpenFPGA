a40d0edbaae7e814f205401121d0b3a789516139
obijuan <obijuan.cube@gmail.com>
2026-04-26 13:23:44 +0200

    log: add example 20: switch_led

---
+++ wiki/log/examples/20-switch-led/switch_led.v
@@ -0,0 +1,15 @@
+`default_nettype none   
+
+
+module switch_led (
+    input wire clk, 
+    input wire [4:0] buttons,
+    input wire switch, 
+    output wire [15:0] leds
+);
+
+//-- Mostrar el siwtch 0 en el led 15
+assign leds[15] = switch;
+
+endmodule
+
