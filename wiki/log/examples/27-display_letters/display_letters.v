a675520e89ccdc5c8b57988b0c866f610fc460c8
obijuan <obijuan.cube@gmail.com>
2026-04-26 23:24:20 +0200

    log: update example 27: display_letters

---
+++ wiki/log/examples/27-display_letters/display_letters.v
@@ -0,0 +1,90 @@
+`default_nettype none   
+
+
+module display_letters (
+    input wire clk, 
+    input wire [4:0] buttons,
+    input wire [15:0] switches, 
+    output wire [15:0] leds,
+    output wire [7:0] segments,
+    output wire [3:0] display_sel
+);
+
+//------ DISPLAY DE 7 SEGMENTOS
+//-- Señales para el usuario, con logica positiva
+wire [1:0] disp_sel; //-- Seleccion del display (0-3)
+wire [7:0] seg;      //-- Segmentos a encender
+
+//-- Mapear las señales del usuario a las reales
+//-- Conexion con el display
+assign segments = ~seg;
+
+//-- Decodificador de 2 a 4, negado
+assign display_sel = ~(1 << disp_sel);
+
+//----------------------------
+//-- PRESCALER
+//----------------------------
+
+prescaler2 #(.N(20)
+) u_press0 (
+    .clk(clk),
+    .signal(gen),  
+);
+
+//-- Generador de señal cuadrada
+wire [1:0] gen;
+
+//-------------------------
+//--       MAIN
+//-------------------------
+
+//-- Seleccionar display
+assign disp_sel = gen;
+
+//---------------------------
+//-- CONVERSORES BCD-7SEG
+//---------------------------
+wire [7:0] seg0;
+bcd_to_7seg u_conv0_bcd2seg (
+    .bcd_in(4'h0),
+    .disp_out(seg0)
+);
+
+wire [7:0] seg1;
+bcd_to_7seg u_conv1_bcd2seg (
+    .bcd_in(4'h0),
+    .disp_out(seg1)
+);
+
+//-- Funcion para obtener el codigo de cada
+//-- letra, a partir de su codigo ASCII
+function [7:0] letra (
+    input [7:0] car
+);
+
+    letra = car-65;
+endfunction
+
+
+//-- Mostrar las letras en el display
+assign seg = letter;
+
+//-- Letras a sacar en el display
+assign code = gen==2'b11 ? letra("H") :
+              gen==2'b10 ? letra("O") :
+              gen==2'b01 ? letra("L") :
+              gen==2'b00 ? letra("A") :
+              8'h0;
+
+//-- Generador de letras
+wire [7:0] letter;
+wire [4:0] code;
+disp_letter u_disp_letter0 (
+    .code_in(code),
+    .letter_out(letter)
+);
+
+
+endmodule
+
