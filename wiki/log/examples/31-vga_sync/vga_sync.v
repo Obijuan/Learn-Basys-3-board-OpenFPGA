4c76b369636ffb7e9ed51b67e28b924da7790e4f
obijuan <obijuan.cube@gmail.com>
2026-04-27 11:58:11 +0200

    log: add example 31: vga_sync

---
+++ wiki/log/examples/31-vga_sync/vga_sync.v
@@ -0,0 +1,135 @@
+`default_nettype none   
+
+module vga_sync (
+    input wire clk, 
+
+    //-- BOTONES
+    input wire [4:0] buttons,
+
+    //-- SWITCHES
+    input wire [15:0] switches,
+
+    //-- LEDS 
+    output wire [15:0] leds,
+
+    //-- DIPLAY 7 SEGMENTOS
+    output wire [7:0] segments,
+    output wire [3:0] display_sel,
+
+    //-- VGA
+    output wire [3:0] vga_red,
+    output wire [3:0] vga_blue,
+    output wire [3:0] vga_green,
+    output wire       vga_hsync,
+    output wire       vga_vsync
+);
+
+//------------------------------------------------------------
+//-- Parametros de la VGA
+localparam LINE_WIDTH = 640;
+localparam LINE_FRONT_PORCH = 16;
+localparam LINE_SYNC_PULSE = 96;
+localparam LINE_BACK_PORCH = 48;
+
+localparam FRAME_HEIGHT = 480;
+localparam FRAME_FRONT_PORCH = 10;
+localparam FRAME_SYNC_PULSE = 2;
+localparam FRAME_BACK_PORCH = 29; //33
+
+//------------------------------------------------------------
+//-- Reloj de la vga: 25Mhz
+wire vga_clk;
+reg [1:0] vga_prescaler = 0;
+always @(posedge clk) begin
+    vga_prescaler <= vga_prescaler + 1;
+end
+
+//-- Este es mi pixel clock
+assign vga_clk = vga_prescaler[1];
+
+//-------------------------------------------------------------
+//-- Sincronizacion
+
+//-- Hay 800 pixeles horizontales en total. De todos ellos solo hay 
+//-- 680 visibles. 800 --> necesitamos 10 bits para representarlo
+//--  --> Las columnas se representan con 10 bits
+reg [9:0] col;  //-- Desde la 0 hasta la 799 (800 en total)
+
+//-- Hay 521 lineas verticales en total, de las cuales solo 480 son visibles
+//-- Necesitamos 9 bits para su representacion
+reg [8:0] row;  //-- Desde 0 hasta 520 (521 en total)
+always @(posedge vga_clk) begin
+    if (col < 799) begin
+        col <= col + 1;
+    end
+    else begin
+        col <= 0;
+
+        //-- Incrementar las filas
+        if (row < 520) begin
+            row <= row + 1;
+        end
+        else begin
+            row <= 0;
+        end
+    end
+end
+
+reg hsync;
+always @(posedge vga_clk) begin
+    if (col < LINE_WIDTH + LINE_FRONT_PORCH) begin
+        hsync <= 1;
+    end
+    else if (col < LINE_WIDTH + LINE_FRONT_PORCH + LINE_SYNC_PULSE) begin
+        hsync <= 0;
+    end
+
+    //-- Back porch
+    else begin
+        hsync <= 1;
+    end
+end
+
+reg vsync;
+always @(posedge vga_clk) begin
+    if (row < FRAME_HEIGHT + FRAME_FRONT_PORCH) begin
+        vsync <= 1;
+    end
+    else if (row < FRAME_HEIGHT + FRAME_FRONT_PORCH + FRAME_SYNC_PULSE) begin
+        vsync <= 0;
+    end
+    else begin
+        vsync <= 1;
+    end
+end
+
+
+//-----------------------------------------------------------
+wire draw;
+assign draw = (col < LINE_WIDTH) && (row < FRAME_HEIGHT);
+
+
+always @(posedge vga_clk) begin
+
+//-- Enviar las señales de sincronizacion a la VGA
+    vga_hsync = hsync;
+    vga_vsync = vsync;
+//--- Establecer colores
+    vga_red   = draw ? 4'hF : 4'h0;
+    vga_blue  = draw ? 4'hF : 4'h0;
+    vga_green = draw ? 4'hF : 4'h0; 
+end
+
+
+
+
+//-- TEST
+assign leds[15] = hsync;
+assign leds[14] = vsync;
+assign leds[13] = draw;
+
+
+
+
+endmodule
+
