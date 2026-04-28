95ac2d174c482b93a0526ef211b8b891ce70884f
obijuan <obijuan.cube@gmail.com>
2026-04-28 12:02:21 +0200

    log: update example 12: button_posedge

--- wiki/log/examples/12-button_posedge/button_posedge.v
+++ wiki/log/examples/12-button_posedge/button_posedge.v
@@ -2,14 +2,15 @@
 
 //-- Contar los flancos de subida en un pulsador
 //-- (Incluidos los que llegan por rebotes)
+
 module button_posedge (
     input wire clk, 
     input wire [4:0] buttons, 
     output wire [15:0] leds
 );
 
-//------- Sincronizador para el pulsador
-//-- Pulsador sincronizado
+//──────── Sincronizador para el pulsador
+//── Pulsador sincronizado
 wire button_sync;
 synchronizer u_sync (
     .clk(clk),
@@ -17,12 +18,12 @@ synchronizer u_sync (
     .sync_out(button_sync)
 );
 
-//---- Detectar flanco de subida en el pulsador
+//──────── Detectar flanco de subida en el pulsador
 wire button_press;
 posedge_detector u_posedge (
     .clk(clk),
     .value(button_sync),
-    .pos_edge(button_press)
+    .tic(button_press)
 );
 
 //-- Contador de pulsaciones
---
+++ wiki/log/examples/12-button_posedge/signals.v
@@ -0,0 +1,55 @@
+//══════════════════════════════════════════════════════════
+//─ Bibloteca de modulos para procesado basico de señales
+//══════════════════════════════════════════════════════════
+
+
+//──────────────────────────────────────────────────────────────────────
+//──  SINCRONIZADOR
+//──────────────────────────────────────────────────────────────────────
+//── Estabilizar las señales de entrada, para evitar problemas
+//── de metaestabilidad
+//──────────────────────────────────────────────────────────────────────
+module synchronizer(
+    input wire clk,
+    input wire async_in,  //-- Entrada asíncrona
+    output wire sync_out  //-- Salida sincronizada
+);
+
+    //-- La señal se pasa por dos biestables en serie
+    reg [1:0] stages;
+    always @(posedge clk) begin
+        stages <= { stages[0], async_in };
+    end
+
+    //-- Salida estabilizada
+    assign sync_out = stages[1];
+endmodule
+
+
+//──────────────────────────────────────────────────────────────────────
+//──  DETECTOR DE FLANCOS DE SUBIDA ↑
+//──────────────────────────────────────────────────────────────────────
+//── Detectar un flanco de subida en la señal de entrada
+//── Se devuelve un 'tic' por cada flanco de subida detectado
+//──────────────────────────────────────────────────────────────────────
+module posedge_detector (
+    input wire clk,
+
+    //-- Valor de entrada
+    input wire value,
+
+    //-- Flanco de subida detectado
+    output wire tic
+);
+
+//-- Valor en el siguiente ciclo
+reg value_r;
+always @( posedge clk ) begin
+    value_r <= value;
+end
+
+//-- Hay flanco de subida si el valor actual es 1
+//-- y el del ciclo anterior era 0
+assign tic = ~value_r & value;
+
+endmodule
