3458b64b79202106def2d9ccb01fcbe6a72007c7
obijuan <obijuan.cube@gmail.com>
2026-04-29 11:43:36 +0200

    log: update example 20: switch-led

--- wiki/log/examples/20-switch_led/switch_led.v
+++ wiki/log/examples/20-switch_led/switch_led.v
@@ -4,12 +4,12 @@
 module switch_led (
     input wire clk, 
     input wire [4:0] buttons,
-    input wire switch, 
+    input wire [15:0] switches, 
     output wire [15:0] leds
 );
 
 //-- Mostrar el siwtch 0 en el led 15
-assign leds[15] = switch;
+assign leds[15] = switches[0];
 
 endmodule
 
