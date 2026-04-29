735c0adfe27e509bb942a11e76be4f284a198cfe
obijuan <obijuan.cube@gmail.com>
2026-04-29 09:58:23 +0200

    log: update example 18: cursor_buttons

--- wiki/log/examples/18-cursor-buttons/upload.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-NAME=cursor_buttons
-
-apio raw -- openFPGALoader --board basys3 --bitstream $NAME.bit
-
--- wiki/log/examples/18-cursor_buttons/cursor_buttons.v
+++ wiki/log/examples/18-cursor_buttons/cursor_buttons.v
@@ -1,4 +1,5 @@
-`default_nettype none   
+`default_nettype none  
+`include "buttons.vh" 
 
 //-- Movimiento de un cursor en los LEDs
 //-- con los botones izquierda-derecha
@@ -10,28 +11,28 @@ module cursor_buttons (
 );
 
 
-//--------------------------------------------
-//-- PULSADORES
-//--------------------------------------------
-wire butt_izq; 
-wire butt_der;
-wire butt_izq_press;
-wire butt_der_press;
-button_input u_btn_izq (
+//──────── PROCESAR PULSADORES
+wire btn_izq; 
+wire bn_der;
+wire btn_izq_press;
+wire btn_der_press;
+
+normal_button u_btn_izq(
     .clk(clk),
-    .button_pin_in(buttons[2]),  //-- Pulsador izquierdo
-    .button_state_out(butt_izq),
-    .press_out(butt_izq_press),
-    .release_out()  //-- Sin conectar
+    .btn_pin(buttons[BTN_LEFT]),  
+    .btn_state(),   //-- No usado
+    .tic_press(btn_izq_press),
+    .tic_release(),  //-- No usado
 );
 
-button_input u_btn_der (
+normal_button u_btn_der(
     .clk(clk),
-    .button_pin_in(buttons[3]),  //-- Pulsador derecho
-    .button_state_out(butt_der),
-    .press_out(butt_der_press),
-    .release_out()  //-- Sin conectar
+    .btn_pin(buttons[BTN_RIGHT]),  
+    .btn_state(),   //-- NO usado
+    .tic_press(btn_der_press),
+    .tic_release(),  //-- No usado
 );
+
 
 //-- Registro que almacena el contenido de los LEDs
 //-- Es una patalla 1x16: 1 file de 16 posiciones
@@ -39,11 +40,11 @@ reg [15:0] screen = 15'h0080;
 always @(posedge clk) begin
 
     //-- Boton derecho: Desplazamiento a la derecha
-    if (butt_der_press)
+    if (btn_der_press)
         screen <= {screen[0], screen[15:1]};
 
     //-- Boton izquierdo: Desplazamiento a la izquierda
-    else if (butt_izq_press)
+    else if (btn_izq_press)
         screen <= {screen[14:0], screen[15]};
 end
 
