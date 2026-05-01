b537f7bb0eda78f7bf23b5c9171fe7a2108010e2
obijuan <obijuan.cube@gmail.com>
2026-05-01 10:50:47 +0200

    log: update example 34: vga_2x2

--- wiki/log/examples/34-vga_2x2/vga_2x2.v
+++ wiki/log/examples/34-vga_2x2/vga_2x2.v
@@ -1,4 +1,6 @@
 `default_nettype none   
+`include "buttons.vh" 
+
 
 module vga_2x2 (
     input wire clk, 
@@ -24,154 +26,56 @@ module vga_2x2 (
     output wire       vga_vsync
 );
 
-//------------------------------------------------------
-//-- BOTONES
-
-//-- Constantes para pulsadores
-localparam CENTER = 0;
-localparam UP = 1;
-localparam DOWN = 4;
-localparam LEFT = 2;
-localparam RIGHT = 3;
-
-wire btn_up;
-wire btn_up_press;
-button_input u_btn0 (
-    .clk(clk),
-    .button_pin_in(buttons[UP]), 
-    .button_state_out(btn_up),
-    .press_out(btn_up_press),
-    .release_out()  //-- Sin conectar
-);
-
-
-//------------------------------------------------------------
-//-- Parametros de la VGA
-localparam LINE_WIDTH = 640;
-localparam LINE_FRONT_PORCH = 16;
-localparam LINE_SYNC_PULSE = 96;
-localparam LINE_BACK_PORCH = 48;
-
-localparam FRAME_HEIGHT = 480;
-localparam FRAME_FRONT_PORCH = 10;
-localparam FRAME_SYNC_PULSE = 2;
-localparam FRAME_BACK_PORCH = 29; //33
-
-//------------------------------------------------------------
-//-- Reloj de la vga: 25Mhz
+//──────────────────────────────────
+//──   VGA VERDE
+//──────────────────────────────────
 wire vga_clk;
-reg [1:0] vga_prescaler = 0;
-always @(posedge clk) begin
-    vga_prescaler <= vga_prescaler + 1;
-end
-
-//-- Este es mi pixel clock
-assign vga_clk = vga_prescaler[1];
-
-
-//-------------------------------------------------------------
-//-- Sincronizacion
-
-//-- Hay 800 pixeles horizontales en total. De todos ellos solo hay 
-//-- 680 visibles. 800 --> necesitamos 10 bits para representarlo
-//--  --> Las columnas se representan con 10 bits
-reg [9:0] col;  //-- Desde la 0 hasta la 799 (800 en total)
-
-//-- Hay 521 lineas verticales en total, de las cuales solo 480 son visibles
-//-- Necesitamos 9 bits para su representacion
-reg [8:0] row;  //-- Desde 0 hasta 520 (521 en total)
-always @(posedge vga_clk) begin
-    if (col < 799) begin
-        col <= col + 1;
-    end
-    else begin
-        col <= 0;
-
-        //-- Incrementar las filas
-        if (row < 520) begin
-            row <= row + 1;
-        end
-        else begin
-            row <= 0;
-        end
-    end
-end
-
-reg hsync;
-always @(posedge vga_clk) begin
-    if (col < LINE_WIDTH + LINE_FRONT_PORCH) begin
-        hsync <= 1;
-    end
-    else if (col < LINE_WIDTH + LINE_FRONT_PORCH + LINE_SYNC_PULSE) begin
-        hsync <= 0;
-    end
-
-    //-- Back porch
-    else begin
-        hsync <= 1;
-    end
-end
-
-reg vsync;
-always @(posedge vga_clk) begin
-    if (row < FRAME_HEIGHT + FRAME_FRONT_PORCH) begin
-        vsync <= 1;
-    end
-    else if (row < FRAME_HEIGHT + FRAME_FRONT_PORCH + FRAME_SYNC_PULSE) begin
-        vsync <= 0;
-    end
-    else begin
-        vsync <= 1;
-    end
-end
-
-
-//-----------------------------------------------------------
-//-- Asignacion de señales a la VGA
-
-//-- Intensidad del verde (0-15)
-localparam INTENSIDAD = 4'h7;
-localparam APAGADO = 4'h0;
-
-//-- Solo hay que asignar color si estamos en la zona visible
-//-- De lo contrario NO se vera nada en la VGA
-//-- draw=1 cuando estamos en la zona visible y 0 en caso contrario
 wire draw;
-assign draw = (col < LINE_WIDTH) && (row < FRAME_HEIGHT);
-
-//-- Fin del frame
-wire end_frame;
-assign end_frame = (row > FRAME_HEIGHT);
-
-//-- Señal de refresco: se ha salido de la zona visible, por tanto
-//-- se puede colocar un nuevo valor en la señal de video para
-//-- el siguiente frame
 wire refresh;
-posedge_detector u_posedge (
-    .clk(clk),
-    .value(end_frame),
-    .pos_edge(refresh)
-);
-
-//-- Enviar las señales de sincronizacion a la VGA
-assign vga_hsync = hsync;
-assign vga_vsync = vsync;
-
-//--- Establecer colores
-assign vga_red   = 4'h0;  //-- Deshabilitado
-assign vga_blue  = 4'h0;  //-- Deshabilitado
-assign vga_green = (video & draw) ? INTENSIDAD : APAGADO;
-
-//----------------------------------------------
+wire video;
 //-- Coordenadas del pixel
 wire [9:0] px;
 wire [8:0] py;
-assign px = draw ? col : 0;
-assign py = draw ? row : 0;
+vga_sync u_vga_sync (
+    .clk(clk),
+    .vga_clk(vga_clk),
 
-//-------------------------------------------------------
-//-- GENERACION DE LA SEÑAL DE VIDEO
+    .px(px),
+    .py(py),
+    .draw(draw),
+    .refresh(refresh),
 
+    .vga_red(vga_red),
+    .vga_blue(vga_blue),
+    .vga_hsync(vga_hsync),
+    .vga_vsync(vga_vsync)
+);
+
+//──────────────────────────────────────────
+//── GENERACION DE LA SEÑAL DE VIDEO VERDE
+//──────────────────────────────────────────
+//--- Señal de video para el color verde
+//-- Solo hay señal en la zona visible de la VGA
+//-- de lo contrario NO hya que enviar señal
+assign vga_green = (video & draw) ? 4'h7 : 4'h0;
+
+//────────────────────────────────────────────
+//── PULSADORES
+//────────────────────────────────────────────
+wire btn_up;
+wire btn_up_press;
+normal_button u_btn0 (
+    .clk(clk),
+    .btn_pin(buttons[BTN_UP]),  
+    .btn_state(btn_up),
+    .tic_press(btn_up_press),
+    .tic_release(),
+);
+
+
+//────────────────────────────────────────────
+//── DIBUJAR
+//────────────────────────────────────────────
 //-- Objeto a dibujar:  Dos regiones cuadradas
 wire cuad0;
 wire cuad1;
@@ -182,7 +86,6 @@ assign cuad1 = (py >= 240) && (px >= 320);
 wire object;
 assign object = cuad0 || cuad1;
                 
-
 //-- Valor estable a pintar. Se captura el estado del pulsador
 //-- Cuando se ha terminado de renderizar el frame
 //-- Actual
@@ -196,11 +99,6 @@ end
 //-- del estado del pulsador
 wire video;
 assign video = (stable) ? ~object : object;
-
-//-- TEST
-assign leds[15] = 1;
-assign leds[14] = btn_up;
-assign leds[9:0] = px;
 
 endmodule
 
