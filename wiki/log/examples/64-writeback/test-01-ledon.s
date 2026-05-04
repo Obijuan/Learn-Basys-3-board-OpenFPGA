3eb735c7083ec2c135a65e433a186ca6df65472c
obijuan <obijuan.cube@gmail.com>
2026-05-04 19:24:30 +0200

    log: add example 64: writeback

---
+++ wiki/log/examples/64-writeback/test-01-ledon.s
@@ -0,0 +1,16 @@
+
+#-- Direccion de los LEDs
+.equ LEDS, 0x200000
+
+.global __reset
+__reset:
+
+    #-- s0: Direccion de los LEDs
+    li s0, LEDS
+
+    #-- Encender led
+    li t0, 1
+    sw t0, (s0)
+
+    #-- STOP
+inf: j inf
