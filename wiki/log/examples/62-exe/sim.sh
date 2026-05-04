1500aba27c6923d085b9d572f66f4bd89799306e
obijuan <obijuan.cube@gmail.com>
2026-05-04 13:55:55 +0200

    log: update example 62: exe

---
+++ wiki/log/examples/62-exe/sim.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+
+#-- Directorio donde esta la librearia
+LIB="../lib"
+
+#-- Dependencias
+DEPS="wishbone_interface.sv \
+      wishbone_leds.sv \
+      synchronizer.sv \
+      wishbone_interconnect.sv \
+      wishbone_buttons.sv \
+      wishbone_switches.sv \
+      uart_tx.sv \
+      uart_rx.sv \
+      wishbone_uart.sv \
+      memory.sv \
+      wishbone_ram.sv \
+      constants.sv \
+      pipeline_status.sv \
+      op.sv \
+      csr.sv \
+      forwarding.sv \
+      instruction.sv \
+      instruction_decoder.sv \
+      fetch_stage.sv \
+      decode_stage.sv \
+      utils.sv \
+      disp7seg.sv \
+      timming.sv \
+      "
+
+#-- Simulacion con verilator
+verilator --binary  --trace-fst -sv --top-module TB -I$LIB \
+  $DEPS top_tb.sv && \
+  ./obj_dir/VTB
+
