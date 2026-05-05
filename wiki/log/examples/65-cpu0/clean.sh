c6fad7f5cd2de372f23efb962181f0c0ed49396e
obijuan <obijuan.cube@gmail.com>
2026-05-05 11:30:40 +0200

    log: update example 65: cpu0

--- wiki/log/examples/65-cpu0/clean.sh
+++ wiki/log/examples/65-cpu0/clean.sh
@@ -3,13 +3,4 @@
 rm -rf _build
 rm -rf obj_dir
 
-rm -f *.bit *.fasm *.frames *.json *.fst *.elf
-rm -rf obj_dir
-rm -f *.dis
-rm -f *.bin
-mv init.mem init.back
-rm -f *.mem
-mv init.back init.mem
-
-
 
