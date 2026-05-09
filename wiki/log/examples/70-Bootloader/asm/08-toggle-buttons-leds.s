24c37afd59dd0a09dab239c83aca45a1acbe6c7a
obijuan <obijuan.cube@gmail.com>
2026-05-09 11:27:14 +0200

    log: 70-Bootloader: example 08-toggle-buttons-leds

--- wiki/log/examples/70-Bootloader/asm/08-toggle-buttons-leds.s
+++ wiki/log/examples/70-Bootloader/asm/08-toggle-buttons-leds.s
@@ -17,44 +17,81 @@ __reset:
    #-- s0: Direccion de los LEDs
    li s0, LEDS_ADDR
 
-   #-- s1: Direccion de los pulsadores
-   li s1, BUTTONS_ADDRESS
-
-   #-- Leer el estado inicial de los pulsadores
-   lw t0, 0(s1)
-
-   #-- t3: Estado de los LEDs
-   li t3, 0
-
    #--- Bucle principal
  main_loop:
-   
-   #-- Leer el estado actual de los pulsadores
-   lw t1, 0(s1)
 
-   #-- Detectar si ha habido pulsación, comparando el estado
-   #-- anterior con el actual
-   xor t2, t0, t1
-   beq t2, zero, next
+   #-- Leer pulsadores de cambio  
+   jal toggle_btn
 
-   #-- Ha habido cambio!
-   #-- Solo nos fijamos en el cambio 0 -> 1: Pulsación!
-   #-- t2 contiene los pulsadores apretados (tic)
-   and t2, t2, t1
-
-   #-- Cambiar el estado de los LEDs afectados
-   xor t3, t3, t2
-   
- next:
-   #-- Mostrar estado en los LEDs
-   sw t3, 0(s0)
-
-   #-- El estado actual pasa a ser el anterior
-   mv t0, t1
+   #-- Enviar su estado a los leds
+   sw a0, 0(s0)
 
    #-- Repetir!
    j main_loop
 
 
 
+#-------------------------------------------------------------
+#-- Lectura de los botones de cambio
+#--
+#-- Se actualiza su estado en funcion de los pulsadores
+#-- y se devuelve su valor listo para usarse
+#--
+#--  SALIDAS:
+#--    a0: Estado de los pulsadores de cambio
+#-------------------------------------------------------------
+toggle_btn:
 
+   #-- Direcciones
+   #--  t0: Variable btn_state
+   #--  t1: Pulsadores
+   la t0, btn_state
+   li t1, BUTTONS_ADDRESS
+
+   #-- Leer el estado anterior de los botones
+   #-- t2: Estado anterior
+   lw t2, 0(t0)
+
+   #-- Leer el estado actual
+   #-- t3: Estado actual
+   lw t3, 0(t1)
+
+   #-- Detectar si ha habido pulsación, comparando el estado
+   #-- anterior con el actual
+   #-- t4: Pulsadores que han cambiado (presion o liberacion)
+   xor t4, t2, t3
+   beq t4, zero, no_change
+
+   #-- Ha habido cambio!
+   #-- Solo nos fijamos en el cambio 0 -> 1: Presion!
+   #-- t5 contiene los pulsadores apretados (tic)
+   and t5, t4, t3
+
+   #-- t6: Botones de cambio
+   lw t6, 4(t0)
+
+   #-- Modificar los botones de cambio que corresponda
+   xor t6, t6, t5
+
+   #-- Guardar botones de cambio
+   sw t6, 4(t0)
+
+ no_change:
+
+   #-- Guardar el estado actual de los pulsadores
+   sw t3, 0(t0)
+
+   #-- Devolver los botones de cambio
+   mv a0, t6
+
+   #-- Terminar
+   ret
+
+
+   .data
+
+#-- Estado anterior de los pulsadores
+btn_state:    .word 0x0000
+
+#-- Estado de los botones de cambio
+toggle_state: .word 0x0000
