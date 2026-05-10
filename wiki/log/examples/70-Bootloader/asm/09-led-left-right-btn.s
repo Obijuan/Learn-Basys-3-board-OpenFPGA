6330d648c719e846705d24d8f280ea68630a810a
obijuan <obijuan.cube@gmail.com>
2026-05-10 15:19:56 +0200

    log: 70-Bootloader: example 09-led-left-right-btn

--- wiki/log/examples/70-Bootloader/asm/09-led-left-right-btn.s
+++ wiki/log/examples/70-Bootloader/asm/09-led-left-right-btn.s
@@ -16,67 +16,53 @@ __reset:
     #-- s0: Direccion de los LEDs
     li s0, LEDS_ADDR
 
-    #-- s1: Direccion de los pulsadores
-    li s1, BUTTONS_ADDRESS
+    #-- s1: particula en los leds
+    li s1, 0x80   #-- Inicialmente en la derecha
 
-    #-- t0: particula en los leds
-    li t0, 1   #-- Inicialmente en la derecha
-
-    #-- Leer estado anterior de los pulsadores
-    lw t1, 0(s1)
 
     #--- Bucle principal
  main_loop:
 
-    #-- Leer estado actual de los pulsadores
-    lw t2, 0(s1)
+    #-- Leer pulsadores
+    jal read_buttons
 
-    #-- Detectar cambio en pulsadores
-    #-- t3: Cambio en pulsadores
-    xor t3, t2, t1
-
-    #-- Detectar presion de tecla
-    and t4, t3, t2
+    #-- a0: Teclas presionadas
 
     #-- Comprobar si tecla izquierda pulsada
-    andi t5, t4, BTN_LEFT
-    beq t5, zero, no_left
+    andi t1, a0, BTN_LEFT
+    beq t1, zero, no_left
 
-    #-- Tecla pulsada!!!
+    #-- Tecla pulsada
 
     #-- Comprobar limite izquierdo
-    li t6, 0x8000
-    beq t0, t6, no_left
+    li t2, 0x8000
+    beq s1, t2, no_left
 
     #-- No se ha llegado al extremo izquierdo
     #-- Desplazar a la izquierda
-    slli t0, t0, 1
+    slli s1, s1, 1
 
  no_left:
 
     #-- Comprobar si tecla derecha pulsada
-    andi t5, t4, BTN_RIGHT
-    beq t5, zero, no_right
+    andi t1, a0, BTN_RIGHT
+    beq t1, zero, no_right
 
     #-- Tecla derecha pulsada!!
 
     #-- Comprobar limite derecho
-    li t6, 0x0001
-    beq t0, t6, no_right
+    li t2, 0x0001
+    beq s1, t2, no_right
 
     #-- Desplazar a la derecha
-    srli t0, t0, 1
+    srli s1, s1, 1
 
  no_right:
 
     #-- Actualizar leds
-    sw t0, 0(s0)
-
-    #-- Guardar estado actual como anterior
-    mv t1, t2   
+    sw s1, 0(s0)
 
     #-- Repetir!
     j main_loop
-
 
 
--- wiki/log/examples/70-Bootloader/asm/buttons.s
+++ wiki/log/examples/70-Bootloader/asm/buttons.s
@@ -1,5 +1,53 @@
     .include "peripherals.h"
 
+
+#------------------------------------------------
+#-- Lectura de los pulsadores
+#--
+#-- SALIDA:
+#--   a0: Pulsadores que se han presionado (tic)
+#------------------------------------------------
+  .global read_buttons
+read_buttons:
+
+    #-- t0: Direccion de los pulsadores
+    li t0, BUTTONS_ADDRESS
+
+    #-- t1: Direccion de la variable: btn_state
+    la t1, btn_state
+
+    #-- Leer Estado actual de los botones
+    #-- t2: Estado actual
+    lw t2, 0(t0)
+
+    #-- Leer estado anterior de los botones
+    #-- t3: Estado anterior
+    lw t3, 0(t1)
+
+    #-- Detectar cambio en pulsadores
+    #-- t4: Cambio en pulsadores
+    xor t4, t3, t2
+
+    #-- Detectar presion de tecla
+    and t5, t4, t2
+
+    #-- Guardar el estado actual de los pulsadores
+    sw t2, 0(t1)
+
+    #-- Devolver las teclas presionadas
+    mv a0, t5
+
+    #-- terminar
+    ret
+
+
+    .data
+
+#-- Estado anterior de los pulsadores
+btn_state: .word 0x0000
+
+
+
 #-------------------------------------------------------------
 #-- Lectura de los botones de cambio
 #--
@@ -9,13 +57,14 @@
 #--  SALIDAS:
 #--    a0: Estado de los pulsadores de cambio
 #-------------------------------------------------------------
-.global toggle_btn
+  .text
+  .global toggle_btn
 toggle_btn:
 
    #-- Direcciones
    #--  t0: Variable btn_state
    #--  t1: Pulsadores
-   la t0, btn_state
+   la t0, btn_state2
    li t1, BUTTONS_ADDRESS
 
    #-- Leer el estado anterior de los botones
@@ -58,10 +107,11 @@ toggle_btn:
    ret
 
 
+
    .data
 
 #-- Estado anterior de los pulsadores
-btn_state:    .word 0x0000
+btn_state2:    .word 0x0000
 
 #-- Estado de los botones de cambio
 toggle_state: .word 0x0000
