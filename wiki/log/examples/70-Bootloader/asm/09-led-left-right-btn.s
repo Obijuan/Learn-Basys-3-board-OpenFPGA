d73109645f8b576cad4d1e49ded0168ebfe7a180
obijuan <obijuan.cube@gmail.com>
2026-05-10 14:54:56 +0200

    log: 70-Bootloader: example 09-led-left-right-btn

--- wiki/log/examples/70-Bootloader/asm/09-led-left-right-btn.s
+++ wiki/log/examples/70-Bootloader/asm/09-led-left-right-btn.s
@@ -3,75 +3,80 @@
  #──  con las teclas LEFT y RIGHT
  #──────────────────────────────────────────────────────
  
-   .include "peripherals.h"
-   .include "so.h"
+    .include "peripherals.h"
+    .include "so.h"
 
 
-   .global __reset
+    .global __reset
 __reset:
 
-   #-- Inicializar la pila
-   la sp, __ram_end
+    #-- Inicializar la pila
+    la sp, __ram_end
 
-   #-- s0: Direccion de los LEDs
-   li s0, LEDS_ADDR
+    #-- s0: Direccion de los LEDs
+    li s0, LEDS_ADDR
 
-   #-- s1: Direccion de los pulsadores
-   li s1, BUTTONS_ADDRESS
+    #-- s1: Direccion de los pulsadores
+    li s1, BUTTONS_ADDRESS
 
-   #-- t0: particula en los leds
-   li t0, 1   #-- Inicialmente en la derecha
+    #-- t0: particula en los leds
+    li t0, 1   #-- Inicialmente en la derecha
 
-   #-- Leer estado anterior de los pulsadores
-   lw t1, 0(s1)
+    #-- Leer estado anterior de los pulsadores
+    lw t1, 0(s1)
 
-   #--- Bucle principal
+    #--- Bucle principal
  main_loop:
 
-   #-- Leer estado actual de los pulsadores
-   lw t2, 0(s1)
+    #-- Leer estado actual de los pulsadores
+    lw t2, 0(s1)
 
-   #-- Detectar cambio en pulsadores
-   #-- t3: Cambio en pulsadores
-   xor t3, t2, t1
+    #-- Detectar cambio en pulsadores
+    #-- t3: Cambio en pulsadores
+    xor t3, t2, t1
 
-   #-- Detectar presion de tecla
-   and t4, t3, t2
+    #-- Detectar presion de tecla
+    and t4, t3, t2
 
-   #-- Comprobar si tecla izquierda pulsada
-   andi t5, t4, BTN_LEFT
-   beq t5, zero, no_left
+    #-- Comprobar si tecla izquierda pulsada
+    andi t5, t4, BTN_LEFT
+    beq t5, zero, no_left
 
-   #-- Tecla pulsada!!!
+    #-- Tecla pulsada!!!
 
-   #-- TODO: comprobar limites!
+    #-- Comprobar limite izquierdo
+    li t6, 0x8000
+    beq t0, t6, no_left
 
-
-   #-- Desplazar a la izquierda
-   slli t0, t0, 1
+    #-- No se ha llegado al extremo izquierdo
+    #-- Desplazar a la izquierda
+    slli t0, t0, 1
 
  no_left:
 
-   #-- Comprobar si tecla derecha pulsada
-   andi t5, t4, BTN_RIGHT
-   beq t5, zero, no_right
+    #-- Comprobar si tecla derecha pulsada
+    andi t5, t4, BTN_RIGHT
+    beq t5, zero, no_right
 
-   #-- Tecla derecha pulsada!!
+    #-- Tecla derecha pulsada!!
 
-   #-- TODO: comprobar limites!!!
-   #-- Desplazar a la derecha
-   srli t0, t0, 1
+    #-- Comprobar limite derecho
+    li t6, 0x0001
+    beq t0, t6, no_right
+
+    #-- Desplazar a la derecha
+    srli t0, t0, 1
 
  no_right:
 
-   #-- Actualizar leds
-   sw t0, 0(s0)
+    #-- Actualizar leds
+    sw t0, 0(s0)
 
-   #-- Guardar estado actual como anterior
-   mv t1, t2   
+    #-- Guardar estado actual como anterior
+    mv t1, t2   
 
-   #-- Repetir!
-   j main_loop
+    #-- Repetir!
+    j main_loop
 
 
 
