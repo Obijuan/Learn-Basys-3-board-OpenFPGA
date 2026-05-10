f5feed37a373631180e2dd0464c5cbd6284c3865
obijuan <obijuan.cube@gmail.com>
2026-05-10 15:37:05 +0200

    log: 70-Bootloader: example 09-led-left-right-btn

--- wiki/log/examples/70-Bootloader/asm/buttons.s
+++ wiki/log/examples/70-Bootloader/asm/buttons.s
@@ -1,5 +1,5 @@
     .include "peripherals.h"
-
+    .include "stack.h"
 
 #------------------------------------------------
 #-- Lectura de los pulsadores
@@ -61,57 +61,35 @@ btn_state: .word 0x0000
   .global toggle_btn
 toggle_btn:
 
-   #-- Direcciones
-   #--  t0: Variable btn_state
-   #--  t1: Pulsadores
-   la t0, btn_state2
-   li t1, BUTTONS_ADDRESS
+   STACK16
 
-   #-- Leer el estado anterior de los botones
-   #-- t2: Estado anterior
-   lw t2, 0(t0)
+    #-- Leer pulsadores
+    jal read_buttons
 
-   #-- Leer el estado actual
-   #-- t3: Estado actual
-   lw t3, 0(t1)
+    #-- a0: Botones apretados
 
-   #-- Detectar si ha habido pulsación, comparando el estado
-   #-- anterior con el actual
-   #-- t4: Pulsadores que han cambiado (presion o liberacion)
-   xor t4, t2, t3
-   beq t4, zero, no_change
-
-   #-- Ha habido cambio!
-   #-- Solo nos fijamos en el cambio 0 -> 1: Presion!
-   #-- t5 contiene los pulsadores apretados (tic)
-   and t5, t4, t3
-
-   #-- t6: Botones de cambio
-   lw t6, 4(t0)
-
-   #-- Modificar los botones de cambio que corresponda
-   xor t6, t6, t5
-
-   #-- Guardar botones de cambio
-   sw t6, 4(t0)
-
- no_change:
-
-   #-- Guardar el estado actual de los pulsadores
-   sw t3, 0(t0)
-
-   #-- Devolver los botones de cambio
-   mv a0, t6
-
-   #-- Terminar
-   ret
+    #-- Direcciones
+    #--  t0: Variable toggle_state
+    la t0, toggle_state
+ 
+    #-- t1: Leer Botones de cambio
+    lw t1, (t0)
+ 
+    #-- Modificar los botones de cambio que corresponda
+    xor t1, t1, a0
+ 
+    #-- Guardar botones de cambio
+    sw t1, (t0)
+ 
+    #-- Devolver los botones de cambio
+    mv a0, t1
+ 
+    #-- Terminar
+    UNSTACK16
 
 
 
-   .data
-
-#-- Estado anterior de los pulsadores
-btn_state2:    .word 0x0000
+    .data
 
 #-- Estado de los botones de cambio
 toggle_state: .word 0x0000
