44880472241ff1c9331c40954e648b13fa01d697
obijuan <obijuan.cube@gmail.com>
2026-05-11 18:59:54 +0200

    log: 70-Bootloader: example-16-timer-chrono

--- wiki/log/examples/70-Bootloader/asm/16-timer-chrono.s
+++ wiki/log/examples/70-Bootloader/asm/16-timer-chrono.s
@@ -5,7 +5,10 @@
     .include "so.h"
     .include "peripherals.h"
 
-.equ TIMER_100ms, 2500000
+#-- El valor real es 2500000
+#-- Pero se ha ajustado empíricamente para que "mas o menos"
+#-- dure 100ms
+.equ TIMER_100ms, 2200000
 
 
 .global __reset
