52de02ed1593aa26acc945478002579640f3d686
obijuan <obijuan.cube@gmail.com>
2026-05-14 19:17:31 +0200

    log: 70-Bootloader: C example: 18-string.s

--- wiki/log/examples/70-Bootloader/asm/18-strings.s
+++ wiki/log/examples/70-Bootloader/asm/18-strings.s
@@ -26,46 +26,46 @@ __reset:
     #j .
 
 
-    la a0, buff
-    li a1, 12
-    li a2, 4
-    li a3, 1
-    jal sprint_uint
-    #jal sprint_hex
+    # la a0, buff
+    # li a1, 12
+    # li a2, 4
+    # li a3, 1
+    # jal sprint_uint
+    # #jal sprint_hex
 
-    la a0, buff
-    jal puts
+    # la a0, buff
+    # jal puts
 
 
     #---- Imprimir numeros de 4 bits
-    # PUTSI "--> Numeros de 4 bits\n"
-    # PUTSI "* Bin4: "
-    # PRINT_BIN4I 0xC
-    # PUTCHARI '\n'
+    PUTSI "--> Numeros de 4 bits\n"
+    PUTSI "* Bin4: "
+    PRINT_BIN4I 0xC
+    PUTCHARI '\n'
 
-    # PUTSI "* Hex4: "
-    # PRINT_HEX4I 0xC
-    # PUTCHARI '\n'
+    PUTSI "* Hex4: "
+    PRINT_HEX4I 0xC
+    PUTCHARI '\n'
 
-    # PUTSI "* Dec4: "
-    # PRINT_UINT4I 0xC
-    # PUTCHARI '\n'
+    PUTSI "* Dec4: "
+    PRINT_UINT4I 0xC
+    PUTCHARI '\n'
 
-    # PUTCHARI '\n'
+    PUTCHARI '\n'
 
-    # #---- Imprimir numeros de 8 bits
-    # PUTSI "--> Numeros de 8 bits\n"
-    # PUTSI "* Bin8: "
-    # PRINT_BIN8I 0x55
-    # PUTCHARI '\n'
+    #---- Imprimir numeros de 8 bits
+    PUTSI "--> Numeros de 8 bits\n"
+    PUTSI "* Bin8: "
+    PRINT_BIN8I 0x55
+    PUTCHARI '\n'
 
-    # PUTSI "* Hex8: "
-    # PRINT_HEX8I 0x55
-    # PUTCHARI '\n'
+    PUTSI "* Hex8: "
+    PRINT_HEX8I 0x55
+    PUTCHARI '\n'
 
-    # PUTSI "* Dec8: "
-    # PRINT_UINT8I 0x55
-    # PUTCHARI '\n'
+    PUTSI "* Dec8: "
+    PRINT_UINT8I 0x55
+    PUTCHARI '\n'
 
     #----- Imprimir numeros de 16 bits
 
