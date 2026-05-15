5a966995fd102f95bb90976ebe4d195e8e92fd76
obijuan <obijuan.cube@gmail.com>
2026-05-15 12:14:01 +0200

    log: 70-Bootloader: C example: 18-string.s

--- wiki/log/examples/70-Bootloader/asm/18-strings.s
+++ wiki/log/examples/70-Bootloader/asm/18-strings.s
@@ -42,62 +42,65 @@ __reset:
     PRINT_UINTI 0xF
     PUTCHARI '\n'
 
-    # PUTCHARI '\n'
+    PUTCHARI '\n'
 
-    # #---- Imprimir numeros de 8 bits
-    # PUTSI "--> Numeros de 8 bits\n"
-    # PUTSI "* Bin8: "
-    # PRINT_BIN8I 0x55
-    # PUTCHARI '\n'
+    #---- Imprimir numeros de 8 bits
+    PUTSI "--> Numeros de 8 bits\n"
+    PUTSI "* Bin8: "
+    PRINT_BIN8I 0x55
+    PUTCHARI '\n'
 
-    # PUTSI "* Hex8: "
-    # PRINT_HEX8I 0x55
-    # PUTCHARI '\n'
+    PUTSI "* Hex8: "
+    PRINT_HEX8I 0x55
+    PUTCHARI '\n'
 
-    # PUTSI "* Dec8: "
-    # PRINT_UINT8I 0x55
-    # PUTCHARI '\n'
+    PUTSI "* Dec8: "
+    PRINT_UINTI 0x55
+    PUTCHARI '\n'
 
-    # PUTSI "* Dec8 (max): "
-    # PRINT_UINT8I 0xFF
-    # PUTCHARI '\n'
+    PUTSI "* Dec8 (max): "
+    PRINT_UINTI 0xFF
+    PUTCHARI '\n'
 
-    # PUTCHARI '\n'
+    PUTCHARI '\n'
 
-    # #----- Imprimir numeros de 16 bits
-    # PUTSI "--> Numeros de 16 bits\n"
-    # PUTSI "* Bin16: "
-    # PRINT_BIN16I 0xAAAA
-    # PUTCHARI '\n'
+    #----- Imprimir numeros de 16 bits
+    PUTSI "--> Numeros de 16 bits\n"
+    PUTSI "* Bin16: "
+    PRINT_BIN16I 0xAAAA
+    PUTCHARI '\n'
 
-    # PUTSI "* Hex16: "
-    # PRINT_HEX16I 0xAAAA
-    # PUTCHARI '\n'
+    PUTSI "* Hex16: "
+    PRINT_HEX16I 0xAAAA
+    PUTCHARI '\n'
 
-    # PUTSI "* Dec16: "
-    # PRINT_UINT8I 0xAAAA
-    # PUTCHARI '\n'
+    PUTSI "* Dec16: "
+    PRINT_UINTI 0xAAAA
+    PUTCHARI '\n'
 
-    # PUTSI "* Dec16 (max): "
-    # PRINT_UINT8I 0xFFFF
-    # PUTCHARI '\n'
+    PUTSI "* Dec16 (max): "
+    PRINT_UINTI 0xFFFF
+    PUTCHARI '\n'
 
-    # PUTCHARI '\n'
+    PUTCHARI '\n'
 
-    # #----- Imprimir numeros de 32 bits
-    # PUTSI "--> Numeros de 32 bits\n"
-    # PUTSI "* Bin32: "
-    # PRINT_BIN32I 0xCAFEBACA
-    # PUTCHARI '\n'
+    #----- Imprimir numeros de 32 bits
+    PUTSI "--> Numeros de 32 bits\n"
+    PUTSI "* Bin32: "
+    PRINT_BIN32I 0xCAFEBACA
+    PUTCHARI '\n'
 
-    # PUTSI "* Hex32: "
-    # PRINT_HEX32I 0xCAFEBACA
-    # PUTCHARI '\n'
+    PUTSI "* Hex32: "
+    PRINT_HEX32I 0xCAFEBACA
+    PUTCHARI '\n'
 
-    # #----- TODO: NO FUNCIONA!!! DEBUG!!!!!!!!!
-    # PUTSI "* Dec32: "
-    # PRINT_UINT32I 0xCAFEBACA
-    # PUTCHARI '\n'
+    PUTSI "* Dec32: "
+    PRINT_UINTI 0xCAFEBACA
+    PUTCHARI '\n'
+
+    PUTSI "* Dec32 (max): "
+    PRINT_UINTI 0xFFFFFFFF
+    PUTCHARI '\n'
 
     halt
 
