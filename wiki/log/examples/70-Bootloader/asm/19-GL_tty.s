43d8506b3bd1568afac228cec57d668566d3c367
obijuan <obijuan.cube@gmail.com>
2026-05-16 20:17:49 +0200

    log: 70-Bootloader: C example: 19-GL_ttys

--- wiki/log/examples/70-Bootloader/asm/19-GL_tty.s
+++ wiki/log/examples/70-Bootloader/asm/19-GL_tty.s
@@ -20,81 +20,22 @@ __reset:
     ANSI_HOME
     ANSI_CLS
 
-    # li a0, 0
-    # li a1, 1  #-- Eliminar 0s iniciales
-    # jal print_uint
-
     GL_GOTOXY 10, 5
     PUTSI "Hola"
 
     GL_GOTOXY 11, 6
     PUTSI "Test"
 
+    GL_SET_PIXEL_RGB_HERE 0x78, 0, 0
+    GL_SET_PIXEL_RGB_HERE 0x73, 0, 0
+    GL_SET_PIXEL_RGB_HERE 0x6E, 0, 0
+
+
     li a0, 0x78  #-- RED
     li a1, 0     #-- GREEN
     li a2, 0     #-- BLUE
     jal GL_setpixelRGBhere
 
-
-
-
-
-
-    # mv s0, a0
-    # mv s1, a1
-    # mv s2, a2
-
-    # #-- Imprimir el codigo ansi
-    # la a0, GL_buff
-    # la a1, ANSI_RGB
-    # jal sprint
-
-    # #-- Color RED
-    # mv a1, s0
-    # li a2, 1   #-- Eliminar espacios iniciales
-    # jal sprint_uint
-
-    # #-- Imprimir ';'
-    # li a1, ';'
-    # jal sprint_char
-
-    # #-- Color GREEN
-    # mv a1, s1
-    # li a2, 1
-    # jal sprint_uint
-
-    # #-- Imprimir ';'
-    # li a1, ';'
-    # jal sprint_char
-
-    # #-- Color BLUE
-    # mv a1, s2
-    # li a2, 1
-    # jal sprint_uint
-
-    # #-- Imprimir 'm'
-    # li a1, ';'
-    # jal sprint_char
-
-    # #-- Imprimir ' '
-    # li a1, ';'
-    # jal sprint_char
-
-    # la a0, GL_buff
-    # jal puts
-    
-#     /**
-#  * \brief Sets the current graphics position
-#  * \param[in] R , G , B the RGB color of the pixel, in [0..255]
-#  * \details Typically used by programs that draw all pixels sequentially,
-#  *  like a raytracer. After each line, one can either printf("\n") or
-#  *  call GL_gotoxy(). If you want to draw individual pixels in an
-#  *  arbitrary order, use GL_setpixelRGB(x,y,R,G,B)
-#  */
-# static inline void GL_setpixelRGBhere(uint8_t R, uint8_t G, uint8_t B) {
-#     // set background color, print space 
-#     printf("\033[48;2;%d;%d;%dm ",(int)R,(int)G,(int)B); 
-# }
 
 
     halt
--- wiki/log/examples/70-Bootloader/asm/GL_tty.h
+++ wiki/log/examples/70-Bootloader/asm/GL_tty.h
@@ -6,3 +6,9 @@
     jal GL_gotoxy
 .endm
 
+.macro GL_SET_PIXEL_RGB_HERE R, G, B
+    li a0, \R  #-- RED
+    li a1, \G  #-- GREEN
+    li a2, \B  #-- BLUE
+    jal GL_setpixelRGBhere
+.endm
