0ce154c5d2fe5d9d336dd0c117933b8a7a131b88
obijuan <obijuan.cube@gmail.com>
2026-06-17 11:23:05 +0200

    verilog: Add examples 20-switch_led and 21-switches_leds

---
+++ verilog/20-switch_led/switch_led.v
@@ -0,0 +1,15 @@
+`default_nettype none   
+
+
+module main (
+    input wire clk, 
+    input wire [4:0] buttons,
+    input wire [15:0] switches, 
+    output wire [15:0] leds
+);
+
+//-- Mostrar el siwtch 0 en el led 15
+assign leds[15] = switches[0];
+
+endmodule
+
