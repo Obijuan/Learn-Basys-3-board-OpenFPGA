e62852b3ad119d39bce4f47918f35f814d2c2f62
obijuan <obijuan.cube@gmail.com>
2026-06-17 10:42:45 +0200

    verilog: Add examples from 6-led_se1 to 11-leds-buttons

---
+++ verilog/10-led_button/led_button.v
@@ -0,0 +1,13 @@
+`default_nettype none
+
+//-- Mostrar el estado de un pulsador en un led
+//-- El boton central se muestra en el LED 15
+
+module main (
+    input wire [4:0] buttons,
+    output wire [15:0] leds,
+);
+
+assign leds[15] = buttons[0];
+
+endmodule
---
+++ verilog/11-leds_buttons/leds_buttons.v
@@ -0,0 +1,21 @@
+`default_nettype none
+
+//-- Mostrar el estado los 5 pulsadores en
+//-- 5 LEDs de mayor peso
+
+module main (
+    input wire [4:0] buttons, 
+    output wire [15:0] leds
+);
+
+//-- Sacar los botones por los leds
+assign leds[15:11] = buttons;
+
+
+//-- BUG!
+//-- Si se asignan valores a los leds restantes
+//-- se obtiene este error
+//-- ERROR: Invalid global constant node 'INT_L_X0Y3/GND_WIRE'
+//assign leds[10:0] = 11'h0;
+
+endmodule
